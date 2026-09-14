63754f7966de8caf12b5cb1edda7512814d74c15
H0lzy73 <dg.holzwarth@gmail.com>
2026-09-14 16:42:54 +0200

    2. Exercise 6.3

---
+++ Week38/Exercise_6_3_Christmas_Sale/Program.cs
@@ -0,0 +1,14 @@
+﻿int time_since_new_year = 21816000;
+double price = 599.95;
+double real_price = 599.95;
+
+while (true) {
+    if (time_since_new_year == 31104000) {
+        real_price = price * 0.7;
+        break;
+    }
+    real_price = price;
+    time_since_new_year++;
+    Console.WriteLine(real_price);
+}
+
