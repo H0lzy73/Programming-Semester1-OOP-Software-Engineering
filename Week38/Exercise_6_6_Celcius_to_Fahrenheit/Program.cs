90ee528ee51406df78696308c1dc4b60f0986ce0
H0lzy73 <dg.holzwarth@gmail.com>
2026-09-14 17:48:58 +0200

    Exercise 6 6 Celcius to Fahrenheit finished

---
+++ Week38/Exercise_6_6_Celcius_to_Fahrenheit/Program.cs
@@ -0,0 +1,12 @@
+﻿double celcius= -5;
+double Fahrenheit=0;
+
+while (celcius <40.5) {
+
+    //Console.WriteLine("{0,10}{1,10}", celcius[DisplayPos], Fahrenheit[DisplayPos]);
+    //Console.WriteLine($"{celcius} | {Fahrenheit}",4);
+    
+    celcius = celcius+0.5;
+    Fahrenheit = 32 + celcius * (9/5); 
+}
+    
