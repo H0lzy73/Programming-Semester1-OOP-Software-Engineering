1e85f7107509db9aee4f285609846c523509c6f5
H0lzy73 <dg.holzwarth@gmail.com>
2026-09-14 17:15:18 +0200

    Exercise 6.5 Holidays exercise finished

---
+++ Week38/Exercise_6_5_Holidays/Program.cs
@@ -0,0 +1,19 @@
+﻿int month = 1;
+
+switch (month) {
+    case 4:
+        Console.WriteLine("Spring Holiday");
+        break;
+    case 7 or 8:
+        Console.WriteLine("Summer Holiday");
+        break;
+    case 10:
+        Console.WriteLine("Autumn Holiday");
+        break;
+    case 12:
+        Console.WriteLine("Christmas Holiday");
+        break;
+    default:
+        Console.WriteLine("Hard Work");
+        break;
+}
